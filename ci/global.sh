ef6fbbf3a0892e2a75774c0c0f2f4b6a980d707a
alanndz <alanmahmud0@gmail.com>
2020-06-18 16:01:54 +0800

    ci/global: fix
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- ci/global.sh
+++ ci/global.sh
@@ -59,9 +59,6 @@ export AK_BRANCH
 export JOBS
 export TZ=":Asia/Makassar"
 
-if [ ! $RELEASE_VERSION ]; then
-    KERNEL_VERSION="1.00"
-fi
 if [ ! $KERNEL_TYPE ]; then
     TYPE_KERNEL="IDK"
 fi
@@ -90,7 +87,7 @@ ZIP_DIR="${TOOLDIR}/AnyKernel3"
 OUTDIR="${KERNELDIR}/.Out"
 IMAGE="${OUTDIR}/arch/arm64/boot/Image.gz-dtb"
 
-if [ "$RELEASE_STATUS" = "true" ]; then
+if [ "$RELEASE_STATUS" = true ]; then
 	KVERSION="${CODENAME}-${RELEASE_VERSION}"
 	ZIP_NAME="${KERNEL_NAME}-${KVERSION}-${DEVICES}-$(date "+%H%M-%d%m%Y").zip"
 else
