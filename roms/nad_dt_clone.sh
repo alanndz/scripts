3a6af3bf2229c74eabbf3b3919c4499256a582b3
alanndz <alanmahmud0@gmail.com>
2020-10-28 11:40:50 +0000

    roms: nad: add cloning hardware/xiaomi/lavender
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- roms/nad_dt_clone.sh
+++ roms/nad_dt_clone.sh
@@ -9,3 +9,6 @@ echo "Cloning device tree and vendor tree ..."
 git clone "$REMOTE/device_xiaomi_lavender.git" device/xiaomi/lavender "${BRANCH:-}"
 git clone "$REMOTE/vendor_xiaomi_lavender.git" vendor/xiaomi/lavender "${BRANCH:-}"
 
+echo "cloning my harware lavender"
+rm -rf hardware/xiaomi
+git clone git@github.com:HANA-CI-Build-Project/hardware_xiaomi_lavender.git hardware/xiaomi/lavender
