01aa222a140e4559295365265c5310ef58dc8b70
alanndz <alanmahmud0@gmail.com>
2021-01-07 07:59:24 +0000

    ci/perf2.sh: update clang flags
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- ci/perf2.sh
+++ ci/perf2.sh
@@ -206,7 +206,7 @@ function compile_clang() {
 		ARCH=arm64 \
 		CC=clang \
 		CLANG_TRIPLE=aarch64-linux-gnu- \
-		CROSS_COMPILE=aarch64-linux-gnu- \
+		CROSS_COMPILE=aarch64-linux-android- \
 		CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
 
