5a48a94d4d0b882c9a8b89cc8c25b5537326d03d
alanndz <alanndz@qmail.id>
2021-05-18 13:14:40 +0000

    kernel: drivers.sh: add more parameter and exfat
    Signed-off-by: alanndz <alanndz@qmail.id>
    Change-Id: Ic482511a966ed1266b7da50c5ed964c57acc7358

--- kernel/drivers.sh
+++ kernel/drivers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Copyright (C) 2019 Rama Bondan Prakoso (rama982)
+# Copyright (C) 2021 alanndz @ github & telegram
 # Scripts to merge / upstream kernel drivers (wifi and audio)
 # for msm-4.4+
+
+cwd=$(pwd)
 
 while (( ${#} )); do
   case ${1} in
@@ -12,9 +15,16 @@ while (( ${#} )); do
        "-d"|"--data") DATA=true ;;
        "-t"|"--tag") shift; TAG=${1} ;;
        "-u"|"--update") UPDATE=true ;;
+       "-C"|"--dir") shift; DIRS=${1} ;;
+       "-e"|"--exfat") EXFAT=true ;;
   esac
   shift
 done
+
+[[ -n ${DIRS} ]] && {
+  echo "Entering ${DIRS}"
+  cd ${DIRS}
+}
 
 [[ -n ${INIT} && -n ${UPDATE} ]] && { echo "Both init and update were specified!"; exit; }
 
@@ -25,6 +35,9 @@ function drivers()
   if [[ $4 == "wlan" ]]; then
      PREFIX="staging"
      NAME="$1/$3"
+  elif [[ $4 == "fs" ]]; then
+     PREFIX=fs
+     NAME=$1
   else
      PREFIX=techpack
      NAME=$1
@@ -69,3 +82,12 @@ if [[ -n ${DATA} ]]; then
   URL_DATA=https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource
   drivers $SUBFOLDER_DATA $URL_DATA $REPO_DATA
 fi
+
+if [[ -n ${EXFAT} ]]; then
+  SUBFOLDER_EXFAT=fs/exfat
+  REPO_EXFAT=( "exfat-linux" )
+  URL_EXFAT=https://github.com/arter97
+  drivers $SUBFOLDER_EXFAT $URL_EXFAT $REPO_EXFAT fs
+fi
+
+[[ -n ${DIRS} ]] && { echo "Entering ${cwd}"; cd ${cwd}; }
